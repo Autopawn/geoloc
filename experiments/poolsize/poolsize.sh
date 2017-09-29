0eab594fab55a9fce3d98e633d871a6ec5b5b56a
Autopawn <franciscojacb@gmail.com>
2017-09-28 21:01:08 -0300

    Finished poolsize experiment and tool to create graphs from summaries.

--- experiments/poolsize/poolsize.sh
+++ experiments/poolsize/poolsize.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -xe
 
+NFACS=50
+MCLIE=100
+MSIZE=1000
+
+FCOST=500
+VGAIN=250
+TCOST=1
+
+POOLSIZE=200
+
 # Create random test case
-python3 ../../tools/problem_generator.py deesu 50 100 1000 500 250 1 problem.txt problem_pos.txt
+python3 ../../tools/problem_generator.py deesu $NFACS $MCLIE $MSIZE $FCOST $VGAIN $TCOST problem.txt problem_pos.txt
 
 # Find optimal solution with lp_solve
 python3 ../../tools/lp_problem_translator.py problem.txt problem.lp
@@ -11,7 +21,7 @@ python3 ../../tools/lp_problem_translator.py problem.txt problem.lp
 rm -rf results || true
 mkdir -p results
 # Compute with many poolsizes and full vision range
-for PSIZE in {1..200}; do
+for PSIZE in {1..$POOLSIZE}; do
     FNAME=$(printf '%04d' "$PSIZE")
     ../../geoloc.exe $PSIZE $PSIZE 1 problem.txt "results/mge_$FNAME.txt"
     ../../geoloc_hd.exe $PSIZE $PSIZE 1 problem.txt "results/hd_$FNAME.txt"
@@ -22,3 +32,6 @@ REGEXV='s/^results\/\(\w\+\)_\([0-9]\+\).*:[^[0-9]]*\([0-9]\+\).*/\1 \2 \3/'
 grep -R "Value:" results | sort | sed -e "$REGEXV" > summary_values.txt
 REGEXT='s/^results\/\(\w\+\)_\([0-9]\+\).*:#.*\([0-9]\+\.[0-9]\+\).*/\1 \2 \3/'
 grep -R "Time:" results | sort | sed -e "$REGEXT" > summary_times.txt
+
+# Create graph
+python3 ../../tools/graph_from_summary.py 'Best solution value v/s $POOL\_SIZE$ \n $|Z|='$NFACS',|C|='$MCLIE',\gamma='$FCOST',\alpha='$VGAIN',\beta='$TCOST'$. Full vision range.' summary_values.txt value_graph.png
---
+++ tools/graph_from_summary.py
@@ -0,0 +1,47 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    if len(sys.argv)!=4:
+        print("Usage: %s <title> <summary_file> <output>"%sys.argv[0])
+    else:
+        points = {}
+
+        inputf = open(sys.argv[2])
+        for li in inputf:
+            li = li.strip()
+            if li=="": continue
+            name,sx,sy = li.split()
+            sx = int(sx)
+            sy = int(sy)
+            if name not in points:
+                points[name] = []
+            points[name].append((sx,sy))
+        inputf.close()
+
+        color = plt.cm.rainbow(np.linspace(0,1,len(points)))
+
+        fig = plt.figure()
+        fig.suptitle(sys.argv[1].replace('\\n','\n'))
+
+        ax = plt.subplot(111)
+
+        i = 0
+        for name in points:
+            col = color[i]
+            i += 1
+            tpoints = np.array(points[name])
+            ptsx = tpoints[:,0]
+            ptsy = tpoints[:,1]
+            ax.plot(ptsx,ptsy,'r-',label=name.upper(),c=col)
+
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.1,
+            box.width, box.height * 0.9])
+
+
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+            ncol=len(points))
+
+        fig.savefig(sys.argv[3])
