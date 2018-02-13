ae66dd75b3a2cd3214240e79547b6d34f6342965
Francisco Casas <franciscojacb@gmail.com>
2018-02-13 19:11:34 -0300

    Minor change

--- exps/allgen/call_01iden.sh
+++ exps/allgen/call_01iden.sh
@@ -2,5 +2,5 @@ source "./vars_01iden.sh"
 
 bash generate.sh 01iden
 
-qsub -N 01iden_lp solve_lp.sh 01iden lp
-qsub -N 01iden_greedy solve_geoloc.sh greedy 1 1
+qsub -N 01iden_lp -v 1=01iden,2=lp "solve_lp.sh"
+qsub -N 01iden_greedy "solve_geoloc.sh" greedy 1 1
